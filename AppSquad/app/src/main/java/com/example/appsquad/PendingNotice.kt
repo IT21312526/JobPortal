668260f16652e0330cf19c59501fc57fe485b61e
Ashen Dissanayake <101706077+ashenDiz@users.noreply.github.com>
2023-05-14 12:12:04 +0530

    Update PendingNotice.kt

--- AppSquad/app/src/main/java/com/example/appsquad/PendingNotice.kt
+++ AppSquad/app/src/main/java/com/example/appsquad/PendingNotice.kt
@@ -9,6 +9,7 @@ class PendingNotice : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pending_notice)
+        
 
         val btngoback = findViewById<Button>(R.id.btnGoFromNoticeToDashboard)
         btngoback.setOnClickListener {
