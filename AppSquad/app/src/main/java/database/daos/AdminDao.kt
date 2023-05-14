b61abba61c4628ac935d6ac0a3f7a575d197630c
Thanish Ahamed <99425476+IT21312526@users.noreply.github.com>
2023-05-14 12:13:01 +0530

    Update AdminDao.kt

--- AppSquad/app/src/main/java/database/daos/AdminDao.kt
+++ AppSquad/app/src/main/java/database/daos/AdminDao.kt
@@ -1,11 +1,6 @@
 package database.daos
 
-import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.Query
-import database.entities.Admin
-import database.entities.Company
+
 
 @Dao
 interface AdminDao {
