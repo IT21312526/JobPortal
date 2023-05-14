325e5565fc7dbbcac009d2360d1f25517380bf9f
Thanish-Ahmd <thanishahamed321@gmail.com>
2023-05-14 11:15:51 +0530

    Id change
    New id

--- AppSquad/app/src/main/java/com/example/appsquad/EditCompanyProfile.kt
+++ AppSquad/app/src/main/java/com/example/appsquad/EditCompanyProfile.kt
@@ -1,20 +1,29 @@
 package com.example.appsquad
 
 import adapters.CompanyAdapter
+import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.TextView
 import database.CompanyDatabase
 import database.entities.Company
 import database.repositories.CompanyRepository
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import java.io.ByteArrayOutputStream
+import java.io.IOException
 import java.net.CookieStore
+
+private const val PICK_IMAGE_REQUEST = 1
+
 
 class EditCompanyProfile : AppCompatActivity() {
     lateinit var cname: EditText
@@ -24,6 +33,7 @@ class EditCompanyProfile : AppCompatActivity() {
     lateinit var cphone: EditText
     lateinit var creg: EditText
     lateinit var cdescription: EditText
+    lateinit var clogo: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,6 +50,7 @@ class EditCompanyProfile : AppCompatActivity() {
             cphone = findViewById(R.id.edtEditCompanyPhone)
             cdescription = findViewById(R.id.edtEditCompanyDescription)
             creg = findViewById(R.id.edtEditCompanyReg)
+            clogo = findViewById(R.id.edtEditCompanyImg)
         val sharedPreferences = this.getSharedPreferences("MySession", MODE_PRIVATE)
         val cookies = sharedPreferences.getString("company", null)
 
@@ -54,6 +65,7 @@ class EditCompanyProfile : AppCompatActivity() {
                 val tvPhone = findViewById<EditText>(R.id.edtEditCompanyPhone)
                 val tvDesc = findViewById<EditText>(R.id.edtEditCompanyDescription)
                 val tvReg = findViewById<EditText>(R.id.edtEditCompanyReg)
+                val btnImg = findViewById<ImageButton>(R.id.edtEditCompanyImg)
                 if (data != null) {
                     tvName.setText(data.name)
                     tvEmail.setText(data.email)
@@ -62,6 +74,9 @@ class EditCompanyProfile : AppCompatActivity() {
                     tvReg.setText(data.regNo)
                     tvPhone.setText(data.phone)
                     tvDesc.setText(data.description)
+
+                    val bitmap = BitmapFactory.decodeByteArray(data.companyImg, 0, data.companyImg.size)
+                    btnImg.setImageBitmap(bitmap)
                 }
             }
 
@@ -75,21 +90,58 @@ class EditCompanyProfile : AppCompatActivity() {
             finish()
         }
 
+        val btnCompImg = findViewById<ImageButton>(R.id.edtEditCompanyImg)
+        btnCompImg.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(intent, PICK_IMAGE_REQUEST)
+        }
+
         val btnUpdate = findViewById<Button>(R.id.btnUpdateCompany)
         btnUpdate.setOnClickListener {
             if (cookies != null) {
-                update(repository , cookies , cname, caddress, cemail, cpassword , cphone ,creg , cdescription)
+                update(repository , cookies , cname, caddress, cemail, cpassword , cphone ,creg , cdescription ,clogo , this)
 
-                val btn = findViewById<Button>(R.id.btnUpdateCompany)
-                btn.text = "Updated"
+
             }
         }
     }
 
-    fun update(repository: CompanyRepository, cookies: String , cname :EditText, caddress:EditText, cemail:EditText, cpassword :EditText , cphone:EditText , creg: EditText , cdes:EditText) {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK) {
+            // Image picked successfully, handle the selected image here
+            val selectedImageUri = data?.data
+            val btnAddImage = findViewById<ImageButton>(R.id.edtEditCompanyImg)
+
+            // Load the image from the URI and set it as the image resource
+            try {
+                val inputStream = contentResolver.openInputStream(selectedImageUri!!)
+                val drawable = Drawable.createFromStream(inputStream, selectedImageUri.toString())
+                btnAddImage.setImageDrawable(drawable)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+
+            // Perform further operations with the selected image URI
+        }
+    }
+
+    fun isValidPhoneNumber(phoneNumber: String): Boolean {
+        val regexPattern = Regex("^[+]?[0-9]{10,13}\$")
+        return regexPattern.matches(phoneNumber)
+    }
+
+    fun isEmailValid(email: String): Boolean {
+        val emailPattern = Regex("[a-zA-Z0-9._-]+@[a-z]+\\.[a-z]+")
+        return email.matches(emailPattern)
+    }
+
+    fun update(repository: CompanyRepository, cookies: String , cname :EditText, caddress:EditText, cemail:EditText, cpassword :EditText , cphone:EditText , creg: EditText , cdes:EditText , clogo:ImageButton , context :Context) {
 
         CoroutineScope(Dispatchers.IO).launch {
             val data = cookies?.let { repository.getCompanyDetail(it.toInt()) }
+
             val id = data?.id?.toInt()
             val cm = cname.text.toString()
             val ca = caddress.text.toString()
@@ -99,16 +151,58 @@ class EditCompanyProfile : AppCompatActivity() {
             val reg = creg.text.toString()
             val des = cdes.text.toString()
             if (id != null) {
-                repository.updateCompany(id,cm, ce , ca, cp , phn , reg, des)
+
+                val drawable: Drawable? = clogo.drawable
+
+                if (drawable != null && drawable is BitmapDrawable) {
+                    val bitmap: Bitmap = drawable.bitmap
+
+                    if (bitmap != null) {
+                        val outputStream = ByteArrayOutputStream()
+
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+                        val imageByteArray = outputStream.toByteArray()
+
+                        if (isValidPhoneNumber(phn)) {
+
+                            if (isEmailValid(ce)) {
+                                repository.updateCompany(id,cm, ce , ca, cp , phn , reg, des , imageByteArray)
+
+                                withContext(Dispatchers.Main) {
+                                    val intent = Intent(context, CompanyProfile::class.java)
+
+                                    startActivity(intent)
+                                    finish()
+                                }
+
+                            }else{
+                                withContext(Dispatchers.Main) {
+                                    cemail.error = "Invalid email address"
+                                }
+                            }
+
+
+                        }else{
+
+                            withContext(Dispatchers.Main) {
+                                cphone.error = "Invalid Phone Number"
+                            }
+
+                        }
+
+
+                    }
+                }
+
             }
 
         }
 //        val btn = findViewById<Button>(R.id.btnUpdateCompany)
 //        btn.text = "Updated"
 
-        var intent = Intent(this, CompanyProfile::class.java)
-        intent.putExtra("updated" , "yes")
-        startActivity(intent)
-        finish()
+//        var intent = Intent(this, CompanyProfile::class.java)
+//        intent.putExtra("updated" , "yes")
+//        startActivity(intent)
+//        finish()
     }
 }
